6abe49ac783d5a7bd56e90cd4ebf0207873c7267
Poulshravani <poulshravani2@gmail.com>
2025-05-07 21:02:29 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using assiment;
+
+namespace assiment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lintA, lintB, lintC;
+
+
+            Console.WriteLine("Enter value for a");
+            lintA = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for b");
+            lintB = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter value for c");
+            lintC = Convert.ToInt32(Console.ReadLine());
+
+
+            int lintSum =SimpleCode.Add(lintA,lintB);
+            lintSum = SimpleCode.Add(lintSum,lintC);
+            int lintSub = SimpleCode.Subtraction(lintA, lintB);
+            int lintMultiply = SimpleCode.Multiply(lintA, lintB);
+            float lintDiv = SimpleCode.Division(lintA, lintB);
+            Console.WriteLine("Addition=" + lintSum);
+            Console.WriteLine("Substaction=" + lintSub);
+            Console.WriteLine("multiplication=" + lintMultiply);
+            Console.WriteLine("Division=" + lintDiv);
+            
+        }
+    }
+}
